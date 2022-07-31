d6dde3e9da0ffedc1239cedc0144e7e57df5f9d3
taeyongkang <ddaeyongee@gmail.com>
2022-07-31 19:08:19 +0900

    [SWA] Paypal 결제기능 추가 완료

--- client/src/components/utils/Paypal.js
+++ client/src/components/utils/Paypal.js
@@ -7,6 +7,8 @@ export default class Paypal extends React.Component {
             // Congratulation, it came here means everything's fine!
             console.log("The payment was succeeded!", payment);
             // You can bind the "payment" object's value to your state or props or whatever here, please see below for sample returned data
+
+            this.props.onSuccess(payment)
         }
 
         const onCancel = (data) => {
@@ -28,8 +30,8 @@ export default class Paypal extends React.Component {
         // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
 
         const client = {
-            sandbox: 'YOUR-SANDBOX-APP-ID',
-            production: 'YOUR-PRODUCTION-APP-ID',
+            sandbox: "Aez0DBzRcJ9fPALNm8bQq0iE-rU-41eysrA5Uv-wL1z9GJICu8yblKB6ndBens8QSYCjXYkW8XKWgvAC",
+            production: "YOUR-PRODUCTION-APP-ID",
         }
         // In order to get production's app-ID, you will have to send your app to Paypal for approval first
         // For sandbox app-ID (after logging into your developer account, please locate the "REST API apps" section, click "Create App"):
--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from 'react'
 import {useDispatch} from 'react-redux'
-import {getCartItems, removeCartItem, removeFromCart} from '../../../_actions/user_actions';
+import {getCartItems, removeCartItem, onSuccessBuy} from '../../../_actions/user_actions';
 import UserCardBlock from './Sections/UserCardBlock';
-import {Empty} from "antd";
+import {Empty, Result} from "antd";
 import Paypal from '../../utils/Paypal';
 
 function CartPage(props) {
@@ -11,6 +11,7 @@ function CartPage(props) {
 
     const [Total, setTotal] = useState(0);
     const [ShowTotal, setShowTotal] = useState(false);
+    const [ShowSuccess, setShowSuccess] = useState(false);
 
     useEffect(() => {
         let cartItems = []
@@ -55,6 +56,20 @@ function CartPage(props) {
             })
     }
 
+    const transactionSuccess = (data) => {
+
+        dispatch(onSuccessBuy({
+            paymentData: data,
+            cartDetail: props.user.cartDetail
+        }))
+            .then(response => {
+                if (response.payload.success) {
+                    setShowTotal(false)
+                    setShowSuccess(true)
+                }
+            })
+    }
+
     return (
         <div style={{width: '85%', margin: '3rem auto'}}>
             <h1> My Cart </h1>
@@ -63,22 +78,27 @@ function CartPage(props) {
             </div>
 
             {ShowTotal ?
-
                 <div style={{marginTop: '3rem'}}>
                     <h2> Total Amount: ${Total}</h2>
                 </div>
-                :
-                <>
-                    <br/>
-                    <Empty description={false}/>
-                </>
+                : ShowSuccess ?
+                    <Result
+                        status="success"
+                        title="상품 결제가 성공적으로 완료 되었습니다."
+                    />
+                    :
+                    <>
+                        <br/>
+                        <Empty description={false}/>
+                    </>
             }
 
-            <Paypal />
-
-
-
-
+            {ShowTotal &&
+                <Paypal
+                    total={Total}
+                    onSuccess={transactionSuccess}
+                />
+            }
 
         </div>
     )
